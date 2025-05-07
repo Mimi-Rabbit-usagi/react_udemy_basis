df44be8189e4deaeccb9d52be7392ee3e8680de5
RabbitMimi <142197813+Mimi-Rabbit-usagi@users.noreply.github.com>
2025-05-07 13:18:40 +0000

    Reactでのイベント、スタイルの扱い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 export const App = () => {
   //Reactの場合関数名は大文字になる
+  const onClickButton = () => alert();
+  const contentStyle = { color: "gray", fontSize: "14px" };
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "green" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
