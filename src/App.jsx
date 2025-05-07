f73ce9678bd08c4332ee80e21c10e30fe8ae0fdb
RabbitMimi <142197813+Mimi-Rabbit-usagi@users.noreply.github.com>
2025-05-07 13:58:23 +0000

    stateの扱い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
   //Reactの場合関数名は大文字になる
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountup = () => {
+    setNum(num + 1);
+  };
   return (
     <>
       <h1 style={{ color: "green" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="#633142">元気です!</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountup}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
